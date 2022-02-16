59484599e8792ef187eba33b5cd15b2547b72085
hashem98 <hashemsmadi98@gmail.com>
2022-02-16 05:45:33 +0200

    it's done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+// import { Switch } from 'react-router';
+
+import Home from './components/Home/Home';
+import Movelist from './components/MovieList/MovieList';
+import  Navbar  from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Navbar/>
+      <Routes>
+        {/* <Switch> */}
+          <Route path='/trending' exact  element={<Home/>} />
+          <Route path='/' exact element={<Movelist/>} />
+        {/* </Switch> */}
+      </Routes>
     </div>
   );
 }
